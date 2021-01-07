ffdad873ddaa60849e6fdcdd6d9e624638b52b10
Andrés <andres.gerz@gmail.com>
2021-01-07 11:06:59 -0300

    I finished 'desafío 1'

---
+++ 1.desafio/script.js
@@ -0,0 +1,10 @@
+var name = prompt("Name: ");
+
+var surname = prompt("Surname: "); 
+
+var age = 2021;
+
+var result = "Welcome " + name + " " + surname + " to JavaScript Course " + 
+              age.toString() + "!"; 
+
+alert(result);
